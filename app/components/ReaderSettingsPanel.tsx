7bc842cebcc7cb3cca98956381501fb2e672693c
Carl <carlhenderliter@yahoo.com>
2026-05-03 15:26:02 -0400

    Fix Strongs verse toggle typing

--- app/components/ReaderSettingsPanel.tsx
+++ app/components/ReaderSettingsPanel.tsx
@@ -305,6 +305,7 @@ export function ReaderSettingsPanel({
       | "showChapterHeadings"
       | "showCompanionVerseTranslation"
       | "showAnnotatedGreekUndertext"
+      | "showVerseStrongs"
       | "showCustomVerseTranslation"
       | "showGreekSurface"
       | "showGreekLemma"
